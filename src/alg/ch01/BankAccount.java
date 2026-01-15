fb3b01a13e6aa43080a674f425273b3619cf08c9
Marat Gabitov <gabitovmt@yandex.ru>
2026-01-15 20:53:19 +0500

    Глава 01. Общие сведения

---
+++ src/alg/ch01/BankAccount.java
@@ -0,0 +1,26 @@
+package alg.ch01;
+
+public class BankAccount {
+    // Баланс счёта
+    private double balance;
+
+    // Конструктор
+    public BankAccount(double openingBalance) {
+        balance = openingBalance;
+    }
+
+    // Внесение средств
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    // Снятие средств
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    // Вывод баланса
+    public void display() {
+        System.out.println("balance=" + balance);
+    }
+}
---
+++ src/alg/ch01/BankApp.java
@@ -0,0 +1,24 @@
+package alg.ch01;
+
+public class BankApp {
+    public static void main(String[] args) {
+        // Создание счёта
+        var ba1 = new BankAccount(100.00);
+
+        System.out.print("Before transactions, ");
+        // Вывод баланса
+        ba1.display();
+
+        // Внесение средств
+        ba1.deposit(74.35);
+        // Снятие средств
+        ba1.withdraw(20.00);
+
+        System.out.print("After transactions, ");
+        // Вывод баланса
+        ba1.display();
+
+        // Before transactions, balance=100.0
+        // After transactions, balance=154.35
+    }
+}
