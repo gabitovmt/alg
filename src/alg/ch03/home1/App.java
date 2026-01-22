3174cd4ea8611e35aa959e06030361444b170576
Marat Gabitov <gabitovmt@yandex.ru>
2026-01-22 11:23:46 +0500

    Глава 03. Упражнения 1, 2, 3

---
+++ src/alg/ch03/home1/App.java
@@ -0,0 +1,44 @@
+package alg.ch03.home1;
+
+import alg.ch03.ex01.ArrayBub;
+import alg.ch03.ex02.ArraySel;
+import alg.ch03.ex03.ArrayIns;
+
+import java.util.Random;
+
+public class App {
+    private static final Random RANDOM = new Random();
+
+    @SuppressWarnings("java:S106")
+    public static void main(String[] args) {
+        int maxSize = 100_000;
+
+        var bubArray = new ArrayBub(maxSize);
+        var selArray = new ArraySel(maxSize);
+        var insArray = new ArrayIns(maxSize);
+
+        for (int i = 0; i < maxSize; i++) {
+            long n = RANDOM.nextLong();
+            bubArray.insert(n);
+            selArray.insert(n);
+            insArray.insert(n);
+        }
+
+        long start = System.currentTimeMillis();
+
+        bubArray.bubbleSort();
+        long step1 = System.currentTimeMillis();
+
+        selArray.selectionSort();
+        long step2 = System.currentTimeMillis();
+
+        insArray.insertionSort();
+        long step3 = System.currentTimeMillis();
+
+        System.out.println("Elements: RANDOM");
+        System.out.println("Size of elements: " + maxSize);
+        System.out.println("Bubble Sort: " + (step1 - start) + " ms");
+        System.out.println("Selection Sort: " + (step2 - step1) + " ms");
+        System.out.println("Insertion Sort: " + (step3 - step2) + " ms");
+    }
+}
